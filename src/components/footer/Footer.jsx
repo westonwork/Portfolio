bca0716b4e1614dcc0bcc767c85b828c1af58727
Emily Weston <emmylombardo@aol.com>
2023-03-27 20:52:37 -0400

    Fixed broken route in Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,7 +10,6 @@ const Footer = () => {
             <li><a href="#">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#experience">Experiences</a></li>
-            <li><a href="#education">Education</a></li>
             <li><a href="#portfolio">Portfolio</a></li>
             <li><a href="#contact">Contact</a></li>
         </ul>
